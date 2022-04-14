4992e041f4fb6d59869900c037d38adf2110a7d4
eric.wong <ericsyw@gmail.com>
2022-04-14 17:56:03 +0800

    Add hashable-1.3.5.0

--- haskell-overlays/reflex-packages/default.nix
+++ haskell-overlays/reflex-packages/default.nix
@@ -212,6 +212,8 @@ in
   OneTuple = self.callHackage "OneTuple" "0.3" {};
   base-orphans = self.callHackage "base-orphans" "0.8.6" {};
 
+  hashable = self.callHackage "hashable" "1.3.5.0" {};
+
   turtle = self.callHackage "turtle" "1.5.20" {};
   constraints = self.callHackage "constraints" "0.12" {};
 
